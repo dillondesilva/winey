f72a33235e567e3592a0cb9881825cffb2721c80
Dillon de Silva <dillon.desilva@geeksdiary.com>
2019-09-16 07:02:31 +1000

    Initial commit. Imported dataset in code as well as necessary libraries

---
+++ winey.py
@@ -0,0 +1,16 @@
+# Imports
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.externals import joblib
+
+# Importing Wine Data Set
+dataset_url = 'http://mlr.cs.umass.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
+data = pd.read_csv(dataset_url)
+
+print (data.head())
