5d55ea3ed24c2bd03c3e131d73b08f2adf289221
Dillon de Silva <dillon.desilva@geeksdiary.com>
2019-09-21 10:38:39 +1000

    Random forest regressor model created and dumped

--- winey.py
+++ winey.py
@@ -13,6 +13,9 @@ from sklearn.externals import joblib
 dataset_url = 'http://mlr.cs.umass.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
 data = pd.read_csv(dataset_url, sep=";")
 
+# Output data
+print(data.head())
+print(data.shape)
 print(data.describe())
 
 # Seperating the target data from training features
@@ -20,16 +23,34 @@ y = data.quality
 X = data.drop('quality', axis=1)
 
 # Splitting data into train and test sets
-
-# Train sets: Sets of data which are used
-# to develop an ML model using a classifier of sorts
-
-# Test sets: Sets of data which are used to
-# test our trained data against
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123, stratify=y)
 
 scaler = preprocessing.StandardScaler().fit(X_train)
 X_train_scaled = scaler.transform(X_train)
 
 print(X_train_scaled.mean(axis=0))
+print(X_train_scaled.std(axis=0))
+
+X_test_scaled = scaler.transform(X_train)
+print(X_test_scaled.mean(axis=0))
+print(X_test_scaled.std(axis=0))
+
+pipeline = make_pipeline(preprocessing.StandardScaler(), 
+                        RandomForestRegressor(n_estimators=100))
+
+hyperparameters = { 'randomforestregressor__max_features' : ['auto', 'sqrt', 'log2'],
+                  'randomforestregressor__max_depth': [None, 5, 3, 1]}
+
+# Performing cross-validation with pipeline
+rf_clf = GridSearchCV(pipeline, hyperparameters, cv=10)
+
+rf_clf.fit(X_train, y_train)
+
+pred = rf_clf.predict(X_test)
+
+print(r2_score(y_test, pred))
+print(mean_squared_error(y_test, pred))
+
+# Dump the random forest regressor model
+# into a file
+joblib.dump(rf_clf, 'rf_regressor.pkl')
